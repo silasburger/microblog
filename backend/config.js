713aa05176810fb2177be6d2330e9553b6be43dd
Silas Burger <ybsilas@gmail.com>
2019-01-14 17:13:34 -0800

    correcting db_uri

--- backend/config.js
+++ backend/config.js
@@ -15,7 +15,7 @@ let DB_URI;
 if (process.env.NODE_ENV === "test") {
   DB_URI = "microblog-test";
 } else {
-  DB_URI  = process.env.DATABASE_URL || 'microblog';
+  DB_URI  = process.env.DATABASE_URL || 'postgresql:///microblog';
 }
 
 
--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
-const client = new Client(`postgresql:///${DB_URI}`);
+const client = new Client(DB_URI);
 
 client.connect();
 
