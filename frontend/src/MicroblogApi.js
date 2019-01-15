072d1950db9a0736c9819d6e897a2c5923c6e401
Silas Burger <ybsilas@gmail.com>
2019-01-14 17:34:33 -0800

    updating database changes

--- frontend/src/MicroblogApi.js
+++ frontend/src/MicroblogApi.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
-// ('/api/posts/:post_id/comments');
-
 export default class MicroblogApi {
   static async getTitles() {
     const res = await axios.get(`${BASE_URL}/api/posts`);
